b9dbb5bd7a2043b5bfc272de2b20d6dbc8fa1ccc
rahul <rahulgurjar824582@gmail.com>
2024-09-27 11:35:06 +0530

    Setup React Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
+import {BrowserRouter ,Route ,Routes} from "react-router-dom"
+import Home from "./pages/home"
+import Product from "./pages/product"
+import Service from "./pages/service"
 function App() {
   return (
     <>
-      <div className="bg-slate-400 text-black">hello</div>
+      <BrowserRouter>
+        
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+        <Route path="/product" element={<Product/>}></Route>
+        <Route path="/Services" element={<Service/>} ></Route>
+          
+          
+        </Routes>
+        
+      </BrowserRouter>
     </>
   )
 }
