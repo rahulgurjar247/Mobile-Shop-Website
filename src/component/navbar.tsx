bdba337cb45194168db5269f8e1abcd9391f3715
rahul <rahulgurjar824582@gmail.com>
2024-10-03 19:41:34 +0530

    Update Navbar design

--- src/component/navbar.tsx
+++ src/component/navbar.tsx
@@ -1,22 +1,27 @@
 // import React from 'react'
 
+import Logo from "./Logo";
+
 const Navbar = () => {
   return (
-       <nav className=" fixed backdrop-blur-md bg-white z-0 w-screen py-2  ">
-            <div className="w-2/3 mx-auto flex justify-between items-center">
-            <div id="logo" className="font-bold font-sans text-red-500 shadow-sm shadow-zinc-900">DhramRaj Mobile</div>            
-                 <div>
-                      <ul className=" flex gap-7 font-mono text-lg [&_li]:cursor-pointer transition-transform duration-500 ease-out  ">
-                         <li className="hover:text-blue-200">Home</li>
-                         <li className="hover:text-blue-200">ABOUT</li>
-                         <li className="hover:text-blue-200">PRODUCTS</li>
-                         <li className="hover:text-blue-200">REPAIR</li>
-                         <li className="hover:text-blue-200">CONTACT US</li>
-                      </ul>
-            </div>
-            </div>
-       </nav>
-  )
-}
+    <nav className=" fixed backdrop-blur-md bg-white z-40 w-screen py-2  ">
+      <div className="w-2/3 mx-auto flex justify-between items-center max-md:min-w-full">
+        <div className="max-md:p-2">
+          <Logo color={"black"} />
+        </div>
+        <div className=" ">
+          <ul className=" flex gap-7 font-mono text-lg [&_li]:cursor-pointer transition-transform duration-500 ease-out max-md:hidden ">
+            <li className="hover:text-blue-200">Home</li>
+            <li className="hover:text-blue-200">ABOUT</li>
+            <li className="hover:text-blue-200">PRODUCTS</li>
+            <li className="hover:text-blue-200">REPAIR</li>
+            <li className="hover:text-blue-200">CONTACT US</li>
+          </ul>
+        </div>
+        <div className="max-md:flex p-2  md:hidden">---</div>
+      </div>
+    </nav>
+  );
+};
 
 export default Navbar
