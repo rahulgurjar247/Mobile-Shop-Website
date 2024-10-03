afa99547196d12bc72ba9dc288d0066ebd37f4ef
rahul <rahulgurjar824582@gmail.com>
2024-10-03 19:44:25 +0530

    Add Item On Product Page

--- src/pages/product.tsx
+++ src/pages/product.tsx
@@ -1,12 +1,18 @@
 // import React from 'react'
+import Card from "../component/Card";
+import data from "../data/MOCK_DATA.json";
 
 const Product = () => {
   return (
-    <div>
-      Product page
+    <div className="pt-16 min-h-screen">
+      <div className="w-4/5 mx-auto flex flex-wrap gap-6 justify-center">
+        {data.map((Item) => {
+          return <Card name={Item.name} category={Item.category} />;
+        })}
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default Product
  
